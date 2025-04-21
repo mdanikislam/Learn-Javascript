406c023eaac11c9887332bbad3960e5e00eb3bba
mdanikislam <anikislam487@gmail.com>
2025-04-21 23:08:38 +0600

    object declare

---
+++ 27_object-declare.js
@@ -0,0 +1,15 @@
+// 1 
+const pen = {
+    brand: 'econo',
+    price:10,
+    color:'black'
+}
+console.log(pen);
+
+// 2
+const pencil = new Object()
+console.log(pencil);
+
+// 3
+const rubber = Object.create({})
+console.log(rubber);
