3edb4e8c6000168d20ca629cb9cd9463868cc569
mdanikislam <anikislam487@gmail.com>
2025-04-21 17:43:57 +0600

    break-continue

---
+++ 18_break-continue.js
@@ -0,0 +1,16 @@
+// break
+/* for(let i=0; i<=10;i++){
+    if(i>5){
+        break;
+    }
+    // console.log(i);
+} */
+
+// continue
+for(let i=0; i<10; i++){
+    if(i % 2 == 1){
+        continue
+    }
+    console.log(i);
+    
+}
