bb378af63f20a310a05e33e8bfa3e6f3b73b8528
mdanikislam <anikislam487@gmail.com>
2025-04-21 15:53:33 +0600

    for loop

---
+++ 15_for-loop.js
@@ -0,0 +1,15 @@
+/* 
+for(statement; statement; statement){
+body
+}
+*/
+for(let i=0; i<=10; i++){
+    console.log(i);
+    
+}
+
+// even number simple way 
+for(let i=0; i<=10; i=i+2){
+    console.log(i);
+    
+}
