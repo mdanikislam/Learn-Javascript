d0c150c85b49ad4c4fbf72509f95f623c529853e
mdanikislam <anikislam487@gmail.com>
2025-04-21 12:57:04 +0600

    while loop more

---
+++ 14_while-loop-2.js
@@ -0,0 +1,26 @@
+/* let num = 1;
+let sum  = 0;
+while(num<=10){
+    console.log(num);
+    sum = sum+num;
+    console.log("sum", sum);
+    
+    num++;
+    
+}
+ */
+
+// even number 
+let num=0;
+while(num<10){
+    console.log(num);
+    num++;
+    if(num %2 ===0){
+        console.log("Even number ", num);
+        
+    }
+    else{
+        console.log("Odd number ", num);
+        
+    }
+}
