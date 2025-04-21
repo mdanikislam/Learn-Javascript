91cb5ddab483b363cdddfe1ca9792599365f5faf
mdanikislam <anikislam487@gmail.com>
2025-04-21 20:17:02 +0600

    objects

---
+++ 22_objects.js
@@ -0,0 +1,15 @@
+/* Primitive
+basic , primary */
+let name= "messi";
+let age= 37;
+let food = ["burger", "pizza", "noodles"];
+
+// objects
+// Non primitive 
+const phone = {
+    brand:"Iphone",
+    color: "Black",
+    storage:"256gb",
+    ram: "8gb"
+}
+console.log(phone);
