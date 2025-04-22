e1bc44ac98ba5f807c52233220a209035362a728
mdanikislam <anikislam487@gmail.com>
2025-04-23 01:27:56 +0600

    math function

---
+++ 34_Math-method.js
@@ -0,0 +1,18 @@
+// Math.round 
+const number = Math.round(20.58);
+console.log(number);
+
+// Math.floor 
+const num2 = Math.floor(30.899);
+console.log(num2);
+
+// Math.ceil 
+const num3 = Math.ceil(25.1);
+console.log(num3);
+
+// Math.random 
+const num4 = Math.random()*10;
+const numrInt = Math.round(num4);
+console.log(numrInt);
+
+
