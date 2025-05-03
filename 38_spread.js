ef3a92bb59bfd5d7c33016589958fb8fda8f4dcd
mdanikislam <anikislam487@gmail.com>
2025-05-03 20:22:58 +0600

    destructuring

---
+++ 38_spread.js
@@ -0,0 +1,8 @@
+const numbers = [5,1,2,4,5,4,8,5,54,47,588];
+console.log(...numbers);
+const arrayMax = Math.max(...numbers);
+console.log(arrayMax);
+
+// use spread operator to copy 
+ 
+
---
+++ 39_object-destructuring.js
@@ -0,0 +1,8 @@
+const actor= {
+    name: 'sharukh-khan',
+    age: '55',
+    phone: '01823291299'
+}
+
+const {age} = actor;
+console.log(age);
