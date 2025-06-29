90ae428a8bb425bca88ebeff1d3f20f5afba710a
mdanikislam <anikislam487@gmail.com>
2025-06-29 11:35:52 -0700

    filter

---
+++ Array/filter.js
@@ -0,0 +1,3 @@
+let number = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let result = number.filter(num => num <10);
+console.log(result);
--- Array/map.js
+++ Array/map.js
@@ -1,3 +1,3 @@
 let num = [1,2,4,5,6];
-let doubleNum = num.map(num=>num * 2);
+let doubleNum = num.map(num=>num * 2); // double all element in the array and return new array
 console.log(doubleNum);
